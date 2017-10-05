8de7b711b999ef4454dece3fda3b5c9515a5ff61
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-10-05 17:13:04 +0200

    botan2 in nix.

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@ let
    pkgs = import <nixpkgs> {};
 in pkgs.stdenv.mkDerivation rec {
   name = "glutin-env";
-  buildInputs = with pkgs; [ cmake gtest clang gcc boost sqlite ];
+  buildInputs = with pkgs; [ cmake gtest clang gcc boost sqlite botan2 ];
 }
