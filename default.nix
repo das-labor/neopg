1f0d2d66793f5b0100db58c39ec1e60a2b84e627
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-10-08 01:02:24 +0200

    Add doxygen documentation.

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@ let
    pkgs = import <nixpkgs> {};
 in pkgs.stdenv.mkDerivation rec {
   name = "glutin-env";
-  buildInputs = with pkgs; [ pkgconfig cmake gtest clang gcc boost sqlite botan2 openldap zlib bzip2 gnutls libusb ];
+  buildInputs = with pkgs; [ pkgconfig cmake gtest clang gcc boost sqlite botan2 openldap zlib bzip2 gnutls libusb doxygen ];
 }
