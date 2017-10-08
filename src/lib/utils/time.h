6e387f5569f40235a3384a743c2e9c3e2fb4609e
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-10-08 16:53:52 +0200

    Add missing files.

---
+++ src/lib/utils/time.h
@@ -0,0 +1,23 @@
+/* Time functions
+   Copyright 2017 Marcus Brinkmann
+
+   NeoPG is released under the Simplified BSD License (see license.txt)
+*/
+
+#ifndef NEOPG_TIME_H__
+#define NEOPG_TIME_H__
+
+#include <time.h>
+
+#define NEOPG_DLL __attribute__((visibility("default")))
+     
+namespace NeoPG {
+
+  /**
+     A replacement for timegm.
+  */
+  time_t NEOPG_DLL timegm(struct tm *tm);
+
+}
+
+#endif
