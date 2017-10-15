40dcde3e35122de8241bc81cbc9aedbe43ce99f8
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-10-15 03:48:48 +0200

    Even more tests.

--- src/tests/openpgp.cpp
+++ src/tests/openpgp.cpp
@@ -45,6 +45,47 @@ TEST(NeoPGTest, openpg_test) {
 
   {
     std::stringstream out;
+    OpenPGP::NewPacketHeader tag(OpenPGP::PacketType::Marker, 32768,
+				 OpenPGP::PacketLengthType::Partial);
+    tag.write(out);
+    ASSERT_EQ(out.str(), "\xca\xef");
+  }
+
+  {
+    std::stringstream out;
+    OpenPGP::NewPacketHeader tag(OpenPGP::PacketType::Marker, 2,
+				 OpenPGP::PacketLengthType::Partial);
+    tag.write(out);
+    ASSERT_EQ(out.str(), "\xca\xe1");
+  }
+
+  {
+    std::stringstream out;
+    OpenPGP::NewPacketHeader tag(OpenPGP::PacketType::Marker, 1,
+				 OpenPGP::PacketLengthType::Partial);
+    tag.write(out);
+    ASSERT_EQ(out.str(), "\xca\xe0");
+  }
+
+  {
+    std::stringstream out;
+    OpenPGP::NewPacketHeader tag(OpenPGP::PacketType::Marker, 65536,
+				 OpenPGP::PacketLengthType::Partial);
+    tag.write(out);
+    ASSERT_EQ(out.str(), "\xca\xf0");
+  }
+
+  {
+    std::stringstream out;
+    OpenPGP::NewPacketHeader tag(OpenPGP::PacketType::Marker, 1693,
+				 OpenPGP::PacketLengthType::TwoOctet);
+    tag.write(out);
+    ASSERT_EQ(out.str(), "\xca\xc5\xdd");
+  }
+
+  /* Similar for old packet format, for comparison.  */
+  {
+    std::stringstream out;
     OpenPGP::OldPacketHeader tag(OpenPGP::PacketType::Marker, 100);
     tag.write(out);
     ASSERT_EQ(out.str(), "\xa8\x64");
