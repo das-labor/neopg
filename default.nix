4ac4280d6bb54816628c3e3e601cc7ab779307e3
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-10-17 09:08:48 +0200

    Fix iconv integration.

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@ let
    pkgs = import <nixpkgs> {};
 in pkgs.stdenv.mkDerivation rec {
   name = "glutin-env";
-  buildInputs = with pkgs; [ pkgconfig cmake gtest clang gcc boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr ];
+  buildInputs = with pkgs; [ pkgconfig cmake gtest clang gcc boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr libiconv ];
 }
