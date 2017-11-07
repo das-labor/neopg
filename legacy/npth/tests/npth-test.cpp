936bc6e74db9a1ea4dfecbf4c2441b623e3fb83e
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-11-07 12:44:36 +0100

    Remove npth fork test.

--- legacy/npth/tests/npth-test.cpp
+++ legacy/npth/tests/npth-test.cpp
@@ -2,14 +2,8 @@
 
 #include "gtest/gtest.h"
 
-  int fork_main(int argc, char* argv[]);
   int mutex_main(int argc, char* argv[]);
   int thread_main(int argc, char* argv[]);
-
-TEST(nPthTest, fork) {
-    int result = fork_main(0, NULL);
-    ASSERT_EQ(result, 0);
-}
 
 TEST(nPthTest, mutex) {
     int result = mutex_main(0, NULL);
