615b6b5e36ebfbae65e48bb9346eb9b58689b980
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-11-17 14:28:18 +0100

    Implement user attribute packet.

---
+++ lib/openpgp/user_attribute_packet.cpp
@@ -0,0 +1,50 @@
+/* OpenPGP format
+   Copyright 2017 The NeoPG developers
+
+   NeoPG is released under the Simplified BSD License (see license.txt)
+*/
+
+#include <neopg/openpgp/header.h>
+#include <neopg/openpgp/user_attribute_packet.h>
+#include <neopg/utils/stream.h>
+
+namespace NeoPG {
+namespace OpenPGP {
+
+void UserAttributePacket::write_body(std::ostream& out) const {
+  CountingStream cnt;
+  write_attribute(cnt);
+  /* We have to add 1 for the subpacket type.  */
+  uint32_t len = 1 + cnt.bytes_written();
+  NewPacketLength subpacket_length(len);
+
+  subpacket_length.write(out);
+  out << (uint8_t)attribute_type();
+  write_attribute(out);
+}
+
+PacketType UserAttributePacket::type() const {
+  return PacketType::UserAttribute;
+}
+
+/* Image Subpacket */
+
+void ImageAttributeSubpacket::write_attribute(std::ostream& out) const {
+  /* Little-endian image header length ("historical accident").  */
+  out << (uint8_t)0x10 << (uint8_t)0x00;
+  /* Image header version.  */
+  out << (uint8_t)0x01;
+  /* Encoding.  */
+  out << (uint8_t)ImageEncoding::JPEG;
+  /* Reserved.  */
+  out << std::string(12, '\x00');
+
+  out.write((char*)m_data.data(), m_data.size());
+}
+
+UserAttributeType ImageAttributeSubpacket::attribute_type() const {
+  return UserAttributeType::Image;
+}
+
+}  // namespace OpenPGP
+}  // namespace NeoPG
