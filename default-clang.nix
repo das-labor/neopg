39e211003dfb4717bc7a9ea9e0b6faf14492da5b
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-11-22 04:20:41 +0100

    Fix NixOS default.nix files.

---
+++ default-clang.nix
@@ -0,0 +1,6 @@
+let
+   pkgs = import <nixpkgs> {};
+in pkgs.clangStdenv.mkDerivation rec {
+  name = "neopg-env";
+  buildInputs = with pkgs; [ pkgconfig llvm cmake gtest clang boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr libiconv ];
+}
--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
    pkgs = import <nixpkgs> {};
 in pkgs.stdenv.mkDerivation rec {
-  name = "glutin-env";
-  buildInputs = with pkgs; [ pkgconfig cmake gtest clang gcc boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr libiconv ];
+  name = "neopg-env";
+  buildInputs = with pkgs; [ pkgconfig cmake gtest gcc boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr libiconv ];
 }
