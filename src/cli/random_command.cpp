ae089f64da57d4138eb22822c5c28d5f52eccdb1
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-11-24 23:08:33 +0100

    Improve neopg random.

--- src/cli/random_command.cpp
+++ src/cli/random_command.cpp
@@ -13,11 +13,14 @@ namespace NeoPG {
 namespace CLI {
 
 void RandomCommand::run() {
-  const int blocksize = 128;
-  Botan::byte block[blocksize];
-  while (true) {
-    NeoPG::Crypto::rng->randomize(block, blocksize);
-    std::cout.write((const char*)block, blocksize);
+  bool infinite = m_cmd.count("count") == 0;
+
+  std::vector<uint8_t> block(4096);
+  while (infinite || m_count > 0) {
+    int next_blocksize = m_count < block.size() ? m_count : block.size();
+    NeoPG::Crypto::rng->randomize(block.data(), next_blocksize);
+    std::cout.write((const char*)block.data(), next_blocksize);
+    m_count -= next_blocksize;
   }
 }
 
