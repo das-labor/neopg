cbd2fc5d7d363cf13b441d0700b513169832a057
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-01-12 01:10:28 +0100

    Update nix-shell files.

--- default-clang.nix
+++ default-clang.nix
@@ -2,5 +2,5 @@ let
    pkgs = import <nixpkgs> {};
 in pkgs.clangStdenv.mkDerivation rec {
   name = "neopg-env";
-  buildInputs = with pkgs; [ pkgconfig llvm cmake gtest clang boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr libiconv curl ];
+  buildInputs = with pkgs; [ pkgconfig llvm cmake gtest clang_4.cc boost sqlite botan2 zlib bzip2 gnutls libusb doxygen pythonPackages.gcovr libiconv curl ];
 }
