06046ccaa762bbff3b289521a5dee0be9a8627f9
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-03-09 18:12:10 +0100

    Add make_unique by Herb Sutter.

--- lib/include/neopg/intern/cplusplus.h
+++ lib/include/neopg/intern/cplusplus.h
@@ -5,6 +5,9 @@
 
 #pragma once
 
+#include <cstdint>
+#include <memory>
+
 // (The following comment makes things happen in Doxygen.)
 /// The NeoPG namespace.
 namespace NeoPG {
@@ -12,6 +15,12 @@ namespace NeoPG {
 // Allow byte literals such as 0xff_b.
 constexpr std::uint8_t operator"" _b(unsigned long long v) {
   return static_cast<std::uint8_t>(v);
+}
+
+// https://herbsutter.com/gotw/_102/
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 }  // namespace NeoPG
