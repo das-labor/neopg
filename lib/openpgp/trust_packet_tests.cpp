886c62fe6d61c63d22b61f64d6834b9161d2bf3b
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-05-16 04:54:45 +0200

    Add parser for TrustPacket.

--- lib/openpgp/trust_packet_tests.cpp
+++ lib/openpgp/trust_packet_tests.cpp
@@ -1,5 +1,5 @@
-// Trust packet tests
-// Copyright 2017 The NeoPG developers
+// OpenPGP trust packet (tests)
+// Copyright 2017-2018 The NeoPG developers
 //
 // NeoPG is released under the Simplified BSD License (see license.txt)
 
@@ -12,15 +12,38 @@
 
 using namespace NeoPG;
 
-TEST(NeopgTest, openpgp_trust_packet_test) {
+TEST(OpenpgpTrustPacket, WriteWithNewHeader) {
+  std::stringstream out;
+  TrustPacket packet;
+  packet.m_data =
+      std::vector<uint8_t>{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  packet.write(out);
+  ASSERT_EQ(out.str(), std::string("\xCC\x08"
+                                   "\x01\x02\x03\x04\x05\x06\x07\x08",
+                                   10));
+}
+
+TEST(OpenpgpTrustPacket, ParseGood) {
+  // Test parser.
+  const auto trust = std::vector<uint8_t>{0x01, 0x02, 0x03, 0x04};
+  ParserInput in{(const char*)trust.data(), trust.size()};
+
   {
-    std::stringstream out;
-    TrustPacket packet;
-    packet.m_data =
-        std::vector<uint8_t>{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-    packet.write(out);
-    ASSERT_EQ(out.str(), std::string("\xCC\x08"
-                                     "\x01\x02\x03\x04\x05\x06\x07\x08",
-                                     10));
+    ParserInput::Mark mark(in);
+    ASSERT_NO_THROW(TrustPacket::create_or_throw(in));
+    ASSERT_EQ(in.position(), trust.size());
   }
+  ASSERT_EQ(in.position(), 0);
+  auto packet = TrustPacket::create(in);
+  ASSERT_NE(packet, nullptr);
+  ASSERT_EQ(packet->m_data, trust);
+}
+
+TEST(OpenpgpTrustPacket, ParseBad) {
+  // Test parser (packet too long)
+  const auto trust = std::vector<uint8_t>(TrustPacket::MAX_LENGTH + 1, 0xff);
+  ParserInput in{(const char*)trust.data(), trust.size()};
+
+  ASSERT_ANY_THROW(TrustPacket::create_or_throw(in));
+  ASSERT_EQ(in.position(), (uint32_t)TrustPacket::MAX_LENGTH);
 }
