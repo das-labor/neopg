79278b7913d0f8dc8b72f1d454b7a96edd3e68cd
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-05-16 04:52:33 +0200

    Add parser for ModificationDetectionCodePacket.

--- lib/openpgp/modification_detection_code_packet.h
+++ lib/openpgp/modification_detection_code_packet.h
@@ -1,13 +1,18 @@
 // OpenPGP MDC packet
-// Copyright 2017 The NeoPG developers
+// Copyright 2017-2018 The NeoPG developers
 //
 // NeoPG is released under the Simplified BSD License (see license.txt)
+
+/// \file
+/// This file contains support for OpenPGP MDC packets.
 
 #pragma once
 
 #include <neopg/packet.h>
+#include <neopg/parser_input.h>
 
-#include <vector>
+#include <array>
+#include <memory>
 
 namespace NeoPG {
 
@@ -20,11 +25,44 @@ namespace NeoPG {
 /// PacketLengthType::OneOctet (this is not enforced by this class, but it is
 /// the default behaviour).
 struct NEOPG_UNSTABLE_API ModificationDetectionCodePacket : Packet {
-  /// The MDC data, must be 20 bytes (SHA-1).
-  std::vector<uint8_t> m_data;
+  /// Create a new mdc packet from \p input.
+  ///
+  /// \param input the parser input to read from
+  ///
+  /// \return pointer to packet or nullptr on error
+  static std::unique_ptr<ModificationDetectionCodePacket> create(
+      ParserInput& input);
 
+  /// Create a new mdc packet from \p input. Throw an exception on error.
+  ///
+  /// \param input the parser input to read from
+  ///
+  /// \return pointer to packet
+  ///
+  /// \throws ParserError
+  static std::unique_ptr<ModificationDetectionCodePacket> create_or_throw(
+      ParserInput& input);
+
+  /// The length of an MDC.
+  static const size_t LENGTH = 20;
+
+  /// The MDC data.
+  std::array<uint8_t, LENGTH> m_mdc;
+
+  /// Write the packet body to the output stream.
+  ///
+  /// \param out the output stream to write to
   void write_body(std::ostream& out) const override;
-  PacketType type() const override;
+
+  /// Return the packet type.
+  ///
+  /// \return the value PacketType::ModificationDetectionCode
+  PacketType type() const noexcept override {
+    return PacketType::ModificationDetectionCode;
+  }
+
+  /// Construct a new trust packet.
+  ModificationDetectionCodePacket() = default;
 };
 
 }  // namespace NeoPG
--- lib/openpgp/modification_detection_code_packet_tests.cpp
+++ lib/openpgp/modification_detection_code_packet_tests.cpp
@@ -12,25 +12,39 @@
 
 using namespace NeoPG;
 
-TEST(NeopgTest, openpgp_modification_detection_code_packet_test) {
-  {
-    // Must be new packet header, so we don't test old.
-    std::stringstream out;
-    ModificationDetectionCodePacket packet;
-    packet.m_data = std::vector<uint8_t>{
-        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a,
-        0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14};
-    packet.write(out);
-    ASSERT_EQ(out.str(), std::string("\xD3\x14"
-                                     "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a"
-                                     "\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14",
-                                     22));
-  }
+TEST(OpenpgpModificationDetectionCodePacket, Write) {
+  // Must be new packet header, so we don't test old.
+  std::stringstream out;
+  ModificationDetectionCodePacket packet;
+  const auto mdc = std::array<uint8_t, 20>{
+      0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a,
+      0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14};
+  packet.m_mdc = mdc;
+  packet.write(out);
+  ASSERT_EQ(out.str(), std::string("\xD3\x14"
+                                   "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a"
+                                   "\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14",
+                                   22));
+}
 
-  {
-    // Failures.
-    std::stringstream out;
-    ModificationDetectionCodePacket packet;
-    ASSERT_THROW(packet.write(out), std::logic_error);
-  }
+TEST(OpenpgpModificationDetectionCodePacket, ParseGood) {
+  // Test parser.
+  const auto mdc = std::array<uint8_t, 20>{
+      0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a,
+      0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14};
+  ParserInput in{mdc.data(), mdc.size()};
+  auto packet = ModificationDetectionCodePacket::create_or_throw(in);
+  ASSERT_EQ(packet->m_mdc, mdc);
+  ASSERT_EQ(in.position(), mdc.size());
+
+  ASSERT_ANY_THROW(ModificationDetectionCodePacket::create_or_throw(in));
+}
+
+TEST(OpenpgpModificationDetectionCodePacket, ParseBad) {
+  // Test parser.
+  const auto mdc = std::array<uint8_t, 19>{
+      0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a,
+      0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13};
+  ParserInput in{mdc.data(), mdc.size()};
+  ASSERT_ANY_THROW(ModificationDetectionCodePacket::create_or_throw(in));
 }
