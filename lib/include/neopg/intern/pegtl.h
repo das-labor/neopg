68bc3509036ceb5c5976f31b13270c743eb1cecf
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-05-16 04:07:15 +0200

    Add ParserInput, ParserPosition and ParserError.

--- lib/include/neopg/intern/pegtl.h
+++ lib/include/neopg/intern/pegtl.h
@@ -5,8 +5,39 @@
 
 #pragma once
 
+#include <neopg/parser_error.h>
+#include <neopg/parser_input.h>
+#include <neopg/parser_position.h>
+
 // Protect our use of PEGTL from other library users.
 #define TAO_PEGTL_NAMESPACE neopg_pegtl
 #include <tao/pegtl.hpp>
 
 namespace pegtl = tao::TAO_PEGTL_NAMESPACE;
+
+namespace NeoPG {
+
+class ParserInput::Impl {
+ public:
+  pegtl::memory_input<> m_input;
+  Impl(const char* data, size_t length, const std::string& source)
+      : m_input{data, length, source} {}
+};
+
+class ParserInput::Mark::Impl {
+ public:
+  pegtl::internal::marker<pegtl::internal::iterator,
+                          pegtl::rewind_mode::REQUIRED>
+      m_mark;
+  Impl(ParserInput& in)
+      : m_mark{in.m_impl->m_input.mark<pegtl::rewind_mode::REQUIRED>()} {}
+};
+
+template <typename Input>
+ParserError parser_error(const std::string& msg, const Input& in) {
+  const pegtl::position in_pos = in.position();
+  ParserPosition pos(in_pos.source, in_pos.byte);
+  return ParserError(msg, pos);
+}
+
+}  // namespace NeoPG
