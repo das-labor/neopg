0087c91514f4d2c047b276a7301cc63014dfd0a4
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-05-16 04:55:34 +0200

    Add parser for UserIdPacket.

--- lib/openpgp/user_id_packet_tests.cpp
+++ lib/openpgp/user_id_packet_tests.cpp
@@ -12,23 +12,48 @@
 
 using namespace NeoPG;
 
-TEST(NeopgTest, openpgp_user_id_packet_test) {
+TEST(OpenpgpUserIdPacket, WriteWithOldHeader) {
+  // Test old packet header.
+  std::stringstream out;
+  UserIdPacket packet;
+  packet.write(out);
+  ASSERT_EQ(out.str(), std::string("\xCD\0", 2));
+}
+
+TEST(OpenpgpUserIdPacket, WriteWithNewHeader) {
+  // Test new packet header.
+  std::stringstream out;
+  UserIdPacket packet;
+  packet.m_content = "John Doe john.doe@example.com";
+  packet.write(out);
+  ASSERT_EQ(out.str(), std::string("\xCD\x1D"
+                                   "John Doe john.doe@example.com",
+                                   2 + packet.m_content.size()));
+}
+
+TEST(OpenpgpUserIdPacket, ParseGood) {
+  // Test parser.
+  auto uid = std::string{"jonny@example.com"};
+  ParserInput in{uid.data(), uid.length()};
   {
-    // Test old packet header.
-    std::stringstream out;
-    UserIdPacket packet;
-    packet.write(out);
-    ASSERT_EQ(out.str(), std::string("\xCD\0", 2));
+    ParserInput::Mark mark(in);
+    ASSERT_NO_THROW(UserIdPacket::create_or_throw(in));
+    ASSERT_EQ(in.position(), uid.length());
   }
 
-  {
-    // Test new packet header.
-    std::stringstream out;
-    UserIdPacket packet;
-    packet.m_content = "John Doe john.doe@example.com";
-    packet.write(out);
-    ASSERT_EQ(out.str(), std::string("\xCD\x1D"
-                                     "John Doe john.doe@example.com",
-                                     2 + packet.m_content.size()));
-  }
+  ASSERT_EQ(in.position(), 0);
+  auto packet = UserIdPacket::create_or_throw(in);
+  ASSERT_NE(packet, nullptr);
+  ASSERT_EQ(packet->m_content, uid);
+
+  // Will never throw, so no failure tests.
+}
+
+TEST(OpenpgpUserIdPacket, ParseBad) {
+  // Test parser (packet too long)
+  const auto uid = std::vector<uint8_t>(UserIdPacket::MAX_LENGTH + 1, 0xff);
+  ParserInput in{(const char*)uid.data(), uid.size()};
+
+  ASSERT_ANY_THROW(UserIdPacket::create_or_throw(in));
+  ASSERT_EQ(in.position(), (uint32_t)UserIdPacket::MAX_LENGTH);
 }
