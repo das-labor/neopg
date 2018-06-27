226fa67d1b43e8190dba7a2b0d6b0a179954adf5
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-06-27 02:53:50 +0200

    Add parser for UserAttributePacket.

--- lib/openpgp/user_attribute_packet.h
+++ lib/openpgp/user_attribute_packet.h
@@ -6,54 +6,49 @@
 #pragma once
 
 #include <neopg/packet.h>
+
+#include <neopg/user_attribute_subpacket.h>
+
 #include <vector>
 
 namespace NeoPG {
 
-enum class NEOPG_UNSTABLE_API UserAttributeType : uint8_t {
-  Image = 0x01,
-  Private_100 = 0x64,
-  Private_101 = 0x65,
-  Private_102 = 0x66,
-  Private_103 = 0x67,
-  Private_104 = 0x68,
-  Private_105 = 0x69,
-  Private_106 = 0x6a,
-  Private_107 = 0x6b,
-  Private_108 = 0x6c,
-  Private_109 = 0x6d,
-  Private_110 = 0x6e,
-};
+/// Representation of an OpenPGP [user
+/// attribute](https://tools.ietf.org/html/rfc4880#section-5.12) packet.
+class NEOPG_UNSTABLE_API UserAttributePacket : public Packet {
+ public:
+  /// The subpackets.
+  std::vector<std::unique_ptr<UserAttributeSubpacket>> m_subpackets;
 
-struct NEOPG_UNSTABLE_API UserAttributePacket : Packet {
+  /// Create a new user attribute packet from \p input.
+  ///
+  /// \param input the parser input to read from
+  ///
+  /// \return pointer to packet or nullptr on error
+  static std::unique_ptr<UserAttributePacket> create(ParserInput& input);
+
+  /// Create a new user attribute packet from \p input. Throw an exception on
+  /// error.
+  ///
+  /// \param input the parser input to read from
+  ///
+  /// \return pointer to packet
+  ///
+  /// \throws ParserError
+  static std::unique_ptr<UserAttributePacket> create_or_throw(
+      ParserInput& input);
+
+  /// Write the packet body to the output stream.
+  ///
+  /// \param out the output stream to write to
   void write_body(std::ostream& out) const override;
-  PacketType type() const override;
 
-  virtual void write_attribute(std::ostream& out) const = 0;
-  virtual UserAttributeType attribute_type() const = 0;
-};
-
-/* Image Attribute Subpacket.  */
-
-enum class NEOPG_UNSTABLE_API ImageEncoding : uint8_t {
-  JPEG = 0x01,
-  Private_100 = 0x64,
-  Private_101 = 0x65,
-  Private_102 = 0x66,
-  Private_103 = 0x67,
-  Private_104 = 0x68,
-  Private_105 = 0x69,
-  Private_106 = 0x6a,
-  Private_107 = 0x6b,
-  Private_108 = 0x6c,
-  Private_109 = 0x6d,
-  Private_110 = 0x6e,
-};
-
-struct NEOPG_UNSTABLE_API ImageAttributeSubpacket : UserAttributePacket {
-  std::vector<uint8_t> m_data;
-  void write_attribute(std::ostream& out) const override;
-  UserAttributeType attribute_type() const override;
+  /// Return the packet type.
+  ///
+  /// \return the value PacketType::UserAttribute
+  PacketType type() const noexcept override {
+    return PacketType::UserAttribute;
+  }
 };
 
 }  // namespace NeoPG
