9fcae2ca1f049560a19c85ebd7b540749f53aec8
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-06-28 15:55:08 +0200

    Throw ParserError in EcdhPublicKeyMaterial (found with afl 2.52b).

--- lib/openpgp/public_key/material/ecdh_public_key_material.cpp
+++ lib/openpgp/public_key/material/ecdh_public_key_material.cpp
@@ -11,24 +11,62 @@
 using namespace NeoPG;
 
 namespace NeoPG {
-namespace public_key_material {
+namespace ecdh_public_key_material {
+
 using namespace pegtl;
 
-struct ecdh_kdf : must<one<(char)0x03>, one<(char)0x01>, any, any> {};
+// Grammar
+struct header_size : one<(char)0x03> {};
+struct header_reserved : one<(char)0x01> {};
+struct hash_algo : any {};
+struct symmetric_algo : any {};
+struct ecdh_kdf
+    : must<header_size, header_reserved, hash_algo, symmetric_algo> {};
 
+// Action
 template <typename Rule>
 struct action : nothing<Rule> {};
 
 template <>
-struct action<ecdh_kdf> {
-  template <typename Input>
-  static void apply(const Input& in, uint8_t& m_hash, uint8_t& m_sym) {
-    m_hash = in.peek_byte(2);
-    m_sym = in.peek_byte(3);
-  }
-};  // namespace public_key_material
+struct action<hash_algo>
+    : bind<EcdhPublicKeyMaterial, uint8_t, &EcdhPublicKeyMaterial::m_hash> {};
 
-}  // namespace public_key_material
+template <>
+struct action<symmetric_algo>
+    : bind<EcdhPublicKeyMaterial, uint8_t, &EcdhPublicKeyMaterial::m_sym> {};
+
+// Control
+template <typename Rule>
+struct control : pegtl::normal<Rule> {
+  static const std::string error_message;
+
+  template <typename Input, typename... States>
+  static void raise(const Input& in, States&&...) {
+    throw parser_error(error_message, in);
+  }
+};
+
+template <>
+const std::string control<header_size>::error_message =
+    "ecdh public key material has invalid header size";
+
+template <>
+const std::string control<header_reserved>::error_message =
+    "ecdh public key material has invalid header reserved octet";
+
+template <>
+const std::string control<hash_algo>::error_message =
+    "ecdh public key material is missing hash algorithm";
+
+template <>
+const std::string control<symmetric_algo>::error_message =
+    "ecdh public key material is missing symmetric algorithm";
+
+template <>
+const std::string control<eof>::error_message =
+    "ecdh public key material has trailing data";
+
+}  // namespace ecdh_public_key_material
 }  // namespace NeoPG
 
 using namespace NeoPG;
@@ -38,8 +76,9 @@ std::unique_ptr<EcdhPublicKeyMaterial> EcdhPublicKeyMaterial::create_or_throw(
   auto data = make_unique<EcdhPublicKeyMaterial>();
   data->m_curve.parse(in);
   data->m_key.parse(in);
-  pegtl::parse<public_key_material::ecdh_kdf, public_key_material::action>(
-      in.m_impl->m_input, data->m_hash, data->m_sym);
+  pegtl::parse<ecdh_public_key_material::ecdh_kdf,
+               ecdh_public_key_material::action,
+               ecdh_public_key_material::control>(in.m_impl->m_input, *data);
   return data;
 }
 
